fd58bef52eecb2ad553cd2bb74b7ffd9a851f7b8
Yulia Samoilovich <juliasam.canada@gmail.com>
2024-11-23 15:54:26 -0500

    move build to api directory

--- api/api.py
+++ api/api.py
@@ -1,15 +1,18 @@
 import time
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
-
-@app.route('/')
-def index():
-    return app.send_static_file('index.html') 
 
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
 
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve_react_app(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    return send_from_directory(app.static_folder, "index.html")
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
