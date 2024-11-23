5499f254b97d83701b64a1b992b708dab277a9e6
Yulia Samoilovich <juliasam.canada@gmail.com>
2024-11-23 16:05:58 -0500

    changed api.py

--- api/api.py
+++ api/api.py
@@ -1,7 +1,7 @@
 import time
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='build', static_url_path="")
 
 @app.route('/api/time')
 def get_current_time():
@@ -15,4 +15,4 @@ def serve_react_app(path):
     return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
