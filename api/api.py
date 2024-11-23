285c9a75ed0cee1faf3877fa102592db04ef4fe9
Yulia Samoilovich <juliasam.canada@gmail.com>
2024-11-23 14:06:40 -0500

    added config for deployment

--- api/api.py
+++ api/api.py
@@ -5,8 +5,11 @@ app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file('index.html') 
 
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
